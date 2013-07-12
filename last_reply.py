45121f66c78d20d5882726fd3d325e1590304d43
SC at nitrous <omegacoleman@gmail.com>
2013-07-12 06:46:37 +0000

    Fix a few bugs

--- last_reply.py
+++ last_reply.py
@@ -4,10 +4,11 @@ import urllib2
 import json
 
 top_id = 0
-top_reply_nr = highest_post_number
+top_reply_nr = 0
 
 def get_top(url):
     global top_id
+    global top_reply_nr
     URL_LATEST = url + "latest.json"
     top = json.load(urllib2.urlopen(URL_LATEST))["topic_list"]["topics"][0]
     if top_id != top["id"]:
